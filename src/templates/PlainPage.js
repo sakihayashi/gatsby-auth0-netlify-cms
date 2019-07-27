b9755284161c2f80b8764a35f6cb11f829b2e7b1
Saki Hayashi <saki.hayashi@codeimmersive.com>
2019-07-27 16:02:58 -0400

    removed navigate

--- src/templates/PlainPage.js
+++ src/templates/PlainPage.js
@@ -1,14 +1,12 @@
 
 import React from "react"
 import { handleAuthentication } from "../utils/auth"
-import navigate from 'gatsby'
 
 const PlainPage = ({ data: { page } }) => {
     console.log('handleAuthentication: ', handleAuthentication());
 
 
     handleAuthentication()
-    navigate("/")
 
     return <p>Loading...</p>
     
