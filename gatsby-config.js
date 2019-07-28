b5cf78c02aa92f512693d68fc1dd2902b770547c
Saki Hayashi <saki.hayashi@codeimmersive.com>
2019-07-28 14:07:57 -0400

    identity widget true

--- gatsby-config.js
+++ gatsby-config.js
@@ -180,7 +180,7 @@ module.exports = {
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
         stylesPath: `${__dirname}/src/cms/admin.css`,
-        enableIdentityWidget: false
+        enableIdentityWidget: true
       }
     },
     'gatsby-plugin-netlify' // make sure to keep it last in the array
